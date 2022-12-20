bd7c5412863a8f5a9cae2a9a3fcb0c4dbb6acc72
vidyagirl <theopticalbird@gmail.com>
2022-12-20 20:36:26 -0300

    eeeeae

--- src/MathTools.hx
+++ src/MathTools.hx
@@ -1,13 +1,11 @@
 package;
 
-import cpp.Float32;
-
 class MathTools {
     public static function lerp(start:Float, end:Float, t:Float) {
         return start * (1 - t) + end * t;
     }
 
-    public static function midpoint(start:Float, end:Float32) {
+    public static function midpoint(start:Float, end:Float) {
         return (start+end) / 2;
     }
 }
